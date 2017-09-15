42fb615721a8764651d7ed6f7dc90ad7c9fa94f2
lacavazan <lucascpds@gmail.com>
2017-09-15 20:53:32 -0300

    Spring projeto inicial

---
+++ server/src/produshow/src/main/java/com/mc437/produshow/ProdushowApplication.java
@@ -0,0 +1,13 @@
+package com.mc437.produshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdushowApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ProdushowApplication.class, args);
+	}
+}
