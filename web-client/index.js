ed78ebb03b0bd21dde6d1d15aeafd563b18115d5
lacavazan <lucascpds@gmail.com>
2017-09-22 19:21:34 -0300

    react initial setup

---
+++ web-client/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import {IntlProvider, FormattedMessage, addLocaleData } from 'react-intl';
+import localeMessages from './src/translations/translationData.json';
+import router from './src/router'
+
+//Adicionar idiomas suportados
+const languages = require('./src/constants/Languages');
+for (var i = 0; i < languages.length; i++) {
+  let locale = require('react-intl/locale-data/'+languages[i]);
+  addLocaleData(locale);
+}
+
+//Detecting UI language
+var localeCode;
+
+if (navigator.languages && navigator.languages[0]){
+  localeCode = navigator.languages[0];
+} else if (navigator.languages) {
+  localeCode = navigator.languages;
+} else if (navigator.userLanguage) {
+  localeCode = navigator.userLanguage
+}
+
+if (!(localeCode in languages)) {
+  localeCode = localeCode.toLowerCase().split(/[_-]+/)[0];
+}
+
+if (!(localeCode in languages)) {
+  localeCode = languages[0];
+}
+
+ReactDOM.render(
+  (
+    <IntlProvider locale={localeCode} messages={localeMessages[localeCode]}>
+      {router}
+    </IntlProvider>
+  )
+  , document.getElementById('app'));
---
+++ web-client/src/router.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+
+export default (
+
+  <BrowserRouter>
+    <div>
+      <Route exact path='/'/>
+    </div>
+  </BrowserRouter>
+
+)
